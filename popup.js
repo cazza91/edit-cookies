9a2d1cb19933ef80a520bb46de86a91ef4e304f8
Fabio Cazzavillan <fabiocazzavillan@gmail.com>
2022-02-22 22:59:51 +0100

    first commit

---
+++ popup.js
@@ -0,0 +1,77 @@
+
+const actualSiteLabel = document.getElementById('actual-site');
+const actualCookieValueLabel = document.getElementById('actual-cookie-value');
+const buttonContainer = document.getElementById('button-container');
+
+const COOKIENAME = "CanaleGruppoMOL";
+const COOKIEURL = "https://www.mutuionline.it";
+
+// The async IIFE is necessary because Chrome <89 does not support top level await.
+(async function initPopupWindow() {
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+    if (tab?.url) {
+        try {
+            let url = new URL(tab.url);
+            actualSiteLabel.innerHTML = url.hostname;
+
+            chrome.cookies.get(
+                {"name": COOKIENAME, "url": COOKIEURL},
+                (cookie) => {
+                    if (cookie) {
+                        actualCookieValueLabel.innerHTML = cookie.value;
+                    } else {
+                        actualCookieValueLabel.innerHTML = "cookie non presente";
+                    }
+                }
+            )
+        } catch {}
+    }
+
+})();
+
+const config = [
+    'Sprint',
+    'Fast01',
+    'Fast02'
+]
+
+function createButton(container, buttonLabel) {
+    // create a new div element
+    const newButton = document.createElement("button");
+    // and give it some content
+    const newContent = document.createTextNode(buttonLabel);
+    // add the text node to the newly created div
+    newButton.appendChild(newContent);
+    // add the newly created element and its content into the DOM
+    document.body.insertBefore(newButton, container);
+}
+
+window.addEventListener('load', () => {
+    // creo i bottoni in base alla config
+    config.map(button => {
+        createButton(buttonContainer, button)
+    })
+})
+
+window.addEventListener('click', (e) => {
+    if (e.target.nodeName === 'BUTTON') {
+        const cookieContentToSet = e.target.innerHTML;
+        chrome.cookies.get(
+            {"name": COOKIENAME, "url": COOKIEURL},
+            (cookie) => {
+                console.log(cookie);
+                if (cookie) {
+                    const cookieUrl = cookie.url;
+                    chrome.cookies.set({
+                        "name": COOKIENAME,
+                        "value": cookieContentToSet,
+                        "url": COOKIEURL
+                    })
+                }
+            }
+        )
+    }
+})
+
+
